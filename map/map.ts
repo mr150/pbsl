46651eb9cfbef2c26353635235ac54a87cc93fc4
mr150 <im150@bk.ru>
2022-07-08 13:01:20 +0300

    refactor(map)!: change map type name to `PbMap`

--- map/map.ts
+++ map/map.ts
@@ -1,12 +1,12 @@
-import { pbMap } from './types';
+import { PbMap } from './types';
 import {
 	l, find, findIndex, setNth, prepand, filter, map, join, List, isEmpty, isFilledList
 } from '../list';
 import { pair, fir, sec, Pair } from '../pair';
 
-export function mkMap<K, V>(entries?: [K, V][]): pbMap<K, V> {
+export function mkMap<K, V>(entries?: [K, V][]): PbMap<K, V> {
 	if(entries === undefined) {
-		return <pbMap<K, V>>l();
+		return <PbMap<K, V>>l();
 	}
 
 	return l(
@@ -14,13 +14,13 @@ export function mkMap<K, V>(entries?: [K, V][]): pbMap<K, V> {
 	);
 }
 
-export function get<K, V>(mp: pbMap<K, V>, key: K): V | undefined {
+export function get<K, V>(mp: PbMap<K, V>, key: K): V | undefined {
 	const elem = find(mp, (item) => fir(item) === key);
 
 	return elem === undefined ? elem : sec(elem);
 }
 
-export function set<K, V>(mp: pbMap<K, V>, key: K, value: V): pbMap<K, V> {
+export function set<K, V>(mp: PbMap<K, V>, key: K, value: V): PbMap<K, V> {
 	const index = findIndex(mp, (item) => fir(item) === key);
 
 	if(index < 0) {
@@ -30,28 +30,28 @@ export function set<K, V>(mp: pbMap<K, V>, key: K, value: V): pbMap<K, V> {
 	return setNth(mp, index, pair(key, value));
 }
 
-export function has<K, V>(mp: pbMap<K, V>, key: K): boolean {
+export function has<K, V>(mp: PbMap<K, V>, key: K): boolean {
 	return find(mp, (item) => fir(item) === key) === undefined ? false : true;
 }
 
-export function remove<K, V>(mp: pbMap<K, V>, ...keys: K[]): pbMap<K, V> {
+export function remove<K, V>(mp: PbMap<K, V>, ...keys: K[]): PbMap<K, V> {
 	return filter(
 		mp,
 		(item) => keys.indexOf(fir(item)) < 0
 	);
 }
 
-export function keys<K, V>(mp: pbMap<K, V>): List<K> {
+export function keys<K, V>(mp: PbMap<K, V>): List<K> {
 	return map(mp, (item) => fir(item));
 }
 
-export function values<K, V>(mp: pbMap<K, V>): List<V> {
+export function values<K, V>(mp: PbMap<K, V>): List<V> {
 	return map(mp, (item) => sec(item));
 }
 
 export function merge<K, V>(
-	firMap: pbMap<K, V>, secMap: pbMap<K, V> | Pair<K, V>
-): pbMap<K, V> {
+	firMap: PbMap<K, V>, secMap: PbMap<K, V> | Pair<K, V>
+): PbMap<K, V> {
 	if(isEmpty(secMap)) {
 		return firMap;
 	}
--- map/types.ts
+++ map/types.ts
@@ -1,4 +1,4 @@
 import type { Pair } from '../pair';
 import type { List } from '../list';
 
-export type pbMap<Key = unknown, Value = unknown> = List<Pair<Key, Value>>;
+export type PbMap<Key = unknown, Value = unknown> = List<Pair<Key, Value>>;
